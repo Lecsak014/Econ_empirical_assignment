eee74d7c17debe30e8752e54b079688074e6eb96
Levente Kiss <levente.kiss2@stud.uni-corvinus.hu>
2025-10-18 14:03:48 +0200

    Update assignment.R

--- assignment.R
+++ assignment.R
@@ -22,8 +22,12 @@ df <- df |>
             gas_avg = mean(gas))
 
 # making chategories based on the company
+
+# data frame to help decide
 count <- df |> group_by(company) |>
   summarise(n = n())
+# we dont need it anymore
+rm(count)
 
 df$company_f <- "Other"
 df$company_f[df$company == "Mol"] <- "Mol"
@@ -33,7 +37,61 @@ df$company_f[df$company == "Omv"] <- "Omv"
 df$company_f <- relevel(as.factor(df$company_f), ref = "Other")
 table(df$company_f)
 
+# data for cars / people
+
+car <- read_excel("Passenger cars per thousand capita_2024.xlsx")
+
+car <- car[,c(2,7)]
+
+colnames(car) <- c("city", "car_per_p")
+
+# In the original dataframe some cities are misspelled
+
+df[df$city == "Fűzesabony",]$city <- "Füzesabony"
+df[df$city == "Pűspökladány",]$city <- "Püspökladány"
+df[df$city == "Fűle",]$city <- "Füle"
+df[df$city == "Fűlöpszállás",]$city <- "Fülöpszállás"
+df[df$city == "Révfűlöp",]$city <- "Révfülöp"
+df[df$city == "Sűkösd",]$city <- "Sükösd"
+df[df$city == "Sűmeg",]$city <- "Sümeg"
+df[df$city == "Tiszafűred",]$city <- "Tiszafüred"
+df <- df[df$city != "Tolna-Mözs",]
+
+# join the new data to the original
+
+df<- left_join(df, car, by = "city")
+summary(df$car_per_p)
+
+df$car_per_p <- as.numeric(df$car_per_p)
+summary(df$car_per_p)
+
+# checking for NAs
+
+df_na <- df[is.na(df$car_per_p), ]
+table(df_na$city)
+
+# It cant find Felcsút, probably because of the ","
+
+df[df$city == "Felcsút",]$car_per_p
+car[car$city == "Felcsút",]$car_per_p
+df[df$city == "Felcsút",]$car_per_p <- 2023.3
+
+df_na <- df[is.na(df$car_per_p), ]
+table(df_na$city)
+# Only Budapest left
+
+car[car$city == "Budapest",]$car_per_p
+df[is.na(df$car_per_p),]$car_per_p <- 429.2
+
+df_na <- df[is.na(df$car_per_p), ]
+table(df_na$city)
+# finally...
+
+rm(car)
+rm(df_na)
+
 #regressions
+
 model1 <- lm( df$diesel_avg~ df$company_f ,data = df)
 summary(model1)
 
@@ -49,9 +107,6 @@ summary(model2)
 #model 2 for gas
 model20 <- lm(df$gas_avg ~ df$highway_dummy + df$budapest_dummy)
 summary(model20)
-
-# ggs
-
 
 
 
