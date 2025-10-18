4b096737ad26e0b8a9f5b0dc6d97a51a39f6d3c9
Lecsak014 <levente.kiss2@stud.uni-corvinus.hu>
2025-10-18 11:28:21 +0200

    Add files via upload

--- assignment.R
+++ assignment.R
@@ -13,13 +13,12 @@ df <- read_excel("2025.04.11-04.17.xlsx")
 
 table(df$Company)
 
+colnames(df) <- c("date", "city", "company", "adress", "diesel", "gas")
 
-
-
-
-# big cica, big madafakin cica
-
-
+df <- df |> 
+  group_by(adress, city, company) |>
+  summarise(diesel_avg = mean(diesel),
+            gas_avg = mean(gas))
 
 
 
