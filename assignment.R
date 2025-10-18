a9d9e0d7238b4b2832ae1737087be3bc6c587791
Levente Kiss <levente.kiss2@stud.uni-corvinus.hu>
2025-10-18 12:53:36 +0200

    Add files via upload

--- assignment.R
+++ assignment.R
@@ -10,22 +10,29 @@ library(readxl)
 
 df <- read_excel("2025.04.11-04.17.xlsx")
 
-<<<<<<< HEAD
 # rename columns, so it is easier to work with
-=======
-table(df$Company)
->>>>>>> 4b096737ad26e0b8a9f5b0dc6d97a51a39f6d3c9
 
 colnames(df) <- c("date", "city", "company", "adress", "diesel", "gas")
+
+# making the weekly average prices
 
 df <- df |> 
   group_by(adress, city, company) |>
   summarise(diesel_avg = mean(diesel),
             gas_avg = mean(gas))
 
-#regressions
-model1 <- lm( df$diesel_avg~ df$company ,data = df)
-summary(model1)
+# making chategories based on the company
+
+count <- df |> group_by(company) |>
+  summarise(n = n())
+
+df$company_f <- "Other"
+df$company_f[df$company == "Mol"] <- "Mol"
+df$company_f[df$company == "Orlen"] <- "Orlen"
+df$company_f[df$company == "Shell"] <- "Shell"
+df$company_f[df$company == "Omv"] <- "Omv"
+df$company_f <- relevel(as.factor(df$company_f), ref = "Other")
+table(df$company_f)
 
 #creating dummy for budapest and highway
 df <- df %>%
@@ -33,29 +40,21 @@ df <- df %>%
     highway_dummy = as.numeric(str_detect(adress, "M[0-9]+")),
     budapest_dummy = as.numeric(str_detect(adress, "Budapest")))
 
-model2 <- lm(df$diesel_avg ~ df$highway_dummy + df$budapest_dummy)
-summary(model2)
+
+
+
+#regressions
+model1 <- lm( df$diesel_avg~ df$company_f ,data = df)
+summary(model1)
+
 
 #model 2 for gas
 model20 <- lm(df$gas_avg ~ df$highway_dummy + df$budapest_dummy)
 summary(model20)
 
 
-
-
-<<<<<<< HEAD
-table(df$company)
-=======
->>>>>>> 4b096737ad26e0b8a9f5b0dc6d97a51a39f6d3c9
-
-
-
-
-
-
-
-
-
+model2 <- lm(df$diesel_avg ~ df$highway_dummy + df$budapest_dummy)
+summary(model2)
 
 
 
